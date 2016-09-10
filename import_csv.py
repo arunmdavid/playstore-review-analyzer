29a0f1524a5d6bf56127a5af9fc2db390e63a38b
Arun David <arundavid.info@gmail.com>
2016-09-10 23:09:06 +0530

    Replacing keywor

--- import_csv.py
+++ import_csv.py
@@ -1,17 +1,17 @@
 import nltk,csv,numpy
 from nltk import sent_tokenize, word_tokenize, pos_tag
 reader = csv.reader(open('/Users/arundavid/dev/ml/review.csv', 'rU'), delimiter= ",",quotechar='|')
-list = {}
+ratings = {}
 counter = 0
 for line in reader:
-  list[counter] = {}
-  list[counter]['id'] = line[5]
-  list[counter]['rating'] = line[9]
-  list[counter]['title'] = line[10]
-  list[counter]['message'] = line[11]
+  ratings[counter] = {}
+  ratings[counter]['id'] = line[5]
+  ratings[counter]['rating'] = line[9]
+  ratings[counter]['title'] = line[10]
+  ratings[counter]['message'] = line[11]
   counter = counter + 1
 
-  # example data: list[0] = {'id': 123, 'rating': 5, 'title': 'Good App', 'message': 'App is very useful' }
+  # example data: ratings[0] = {'id': 123, 'rating': 5, 'title': 'Good App', 'message': 'App is very useful' }
 
-for rating in list:
-  print(' =====>>>>> ' + list[rating]['rating'] + ' >> ' + list[rating]['title'] + ' >> ' + list[rating]['message'])
+for rating in ratings:
+  print(' =====>>>>> ' + ratings[rating]['rating'] + ' >> ' + ratings[rating]['title'] + ' >> ' + ratings[rating]['message'])
